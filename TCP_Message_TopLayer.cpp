c2df63973055720009125e8539a7fabe130c0b06
Tyler Piron <125412782+Tyler-Piron@users.noreply.github.com>
2023-04-20 14:25:04 -0500

    Add files via upload

---
+++ TCP_Message_TopLayer.cpp
@@ -0,0 +1,9 @@
+// TCP_Message_TopLayer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	//This Function exists solely to be use in inheritance in comination with the other layers in the stack.
+}
