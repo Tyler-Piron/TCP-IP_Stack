813479d7e4bd7dfd62e489800387ba79fc6e564d
Cole Eggert <125523277+ColeEggert@users.noreply.github.com>
2023-04-23 18:16:14 -0500

    Update StringToBinary.h

--- StringToBinary.h
+++ StringToBinary.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <bitset>
+#include <iostream>
+class StringToBinary {
+public:
+	StringToBinary(const std::string& input) : input(input) {}
+
+	void Convert() {
+		for (char c : input) {
+			std::bitset<8> binary(c);
+			std::cout << binary << " ";
+		}
+		std::cout << std::endl;
+	}
+
+private:
+	std::string input;
+};
