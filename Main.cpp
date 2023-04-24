93d54b1659045b14eb9c6374dafba19e44b4522e
ukaleel <125523274+ukaleel@users.noreply.github.com>
2023-04-24 12:31:43 -0500

    Added the queue and encapsulation implementation

--- Main.cpp
+++ Main.cpp
@@ -1,42 +1,68 @@
 
-#include "ApplicationLayer.cpp"
+#include "ApplicationLayer.h"
 #include "DataLinkLayer.h"
 #include "NetworkandInternetLayer.h"
 #include "TransportLayer.h"
 #include <stack>
+#include <queue>
+#include "TCP_Message_Toplayer.h"
+#include "StringToBinary.h"
 
-// String implementaion of the stack
+
 
 
 int main() {
 	
-	std::stack<std::string> TCPIPStack;
+	//std::stack<std::string> TCPIPStack;
+	
+	std::queue<TCP_Message_TopLayer> TCPQueue;
+
 	ApplicationLayer testingLayer{};
-	IPAddress IPadresses;
-	MacAddress Macadresses;
-	std::string conversionString;
+	testingLayer.setHeader();
+	testingLayer.setRequestLine();
+	TCPQueue.push(testingLayer);
+	
+
+
+	TransportLayer testingTransport(testingLayer);
+	TCPQueue.pop();
+	TCPQueue.push(testingTransport);
+
+	IPAddress networkLayer(testingTransport);
+	TCPQueue.pop();
+	TCPQueue.push(networkLayer);
+	
+	MacAddress dataLinkLayer(networkLayer);
+	TCPQueue.pop();
+	TCPQueue.push(dataLinkLayer);
+
+	dataLinkLayer.displayFrameFormat();
+
+	//IPAddress IPadresses;
+	//MacAddress Macadresses;
+	//std::string conversionString;
 
 	
-	testingLayer.readFile();
-	TCPIPStack.push(testingLayer.getApplicationLayer());
-	PortAssignment myPortAssignment(testingLayer.getApplicationLayer());
-	myPortAssignment.assignPorts();
-	TCPIPStack.push(myPortAssignment.getComboPorts());
-	IPadresses.setSourceandDestinationIP();
-	TCPIPStack.push(IPadresses.getComboIPs());
-	Macadresses.setSourceandDestinationMacAddress();
-	TCPIPStack.push(Macadresses.getComboMacs());
+	//testingLayer.httpHeader.readFile();
+	//TCPIPStack.push(testingLayer.httpHeader.getApplicationLayer());
+	//PortAssignment myPortAssignment(testingLayer.httpHeader.getApplicationLayer());
+	//myPortAssignment.assignPorts();
+	//TCPIPStack.push(myPortAssignment.getComboPorts());
+	//IPadresses.setSourceandDestinationIP();
+	//TCPIPStack.push(IPadresses.getComboIPs());
+	//Macadresses.setSourceandDestinationMacAddress();
+	//TCPIPStack.push(Macadresses.getComboMacs());
 
-	// I could make a function for this
-	// Conversion string is the variable holding all the information in one variable format
-	while (!TCPIPStack.empty()) {
-		//Original line I used for printing before switching to conversionString
-		//std::cout << TCPIPStack.top() << " ";
-		conversionString = conversionString + TCPIPStack.top() + " " + "\n";
-		TCPIPStack.pop();
-	}
-	// Use to print the conversion string as you need for testing purposes and end result
-	std::cout <<  conversionString;
+	//// I could make a function for this
+	//// Conversion string is the variable holding all the information in one variable format
+	//while (!TCPIPStack.empty()) {
+	//	//Original line I used for printing before switching to conversionString
+	//	//std::cout << TCPIPStack.top() << " ";
+	//	conversionString = conversionString + TCPIPStack.top() + " " + "\n";
+	//	TCPIPStack.pop();
+	//}
+	//// Use to print the conversion string as you need for testing purposes and end result
+	//std::cout <<  conversionString;
 
 
 }
