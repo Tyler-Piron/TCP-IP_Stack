9c0dc8003c5dcdd10b1df453f8a0b71ac44b773a
ukaleel <125523274+ukaleel@users.noreply.github.com>
2023-04-24 12:31:06 -0500

    Copy pasted wrong code

--- ApplicationLayer.h
+++ ApplicationLayer.h
@@ -1,68 +1,39 @@
-
-#include "ApplicationLayer.h"
-#include "DataLinkLayer.h"
-#include "NetworkandInternetLayer.h"
-#include "TransportLayer.h"
-#include <stack>
-#include <queue>
-#include "TCP_Message_Toplayer.h"
-#include "StringToBinary.h"
-
+#pragma once
+#include <map>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include "HTTPRequestLine.h"
+#include "HTTPHeader.h"
+#include "TCP_Message_TopLayer.h"
 
 
 
-int main() {
-	
-	//std::stack<std::string> TCPIPStack;
-	
-	std::queue<TCP_Message_TopLayer> TCPQueue;
+class ApplicationLayer : public TCP_Message_TopLayer {
 
-	ApplicationLayer testingLayer{};
-	testingLayer.setHeader();
-	testingLayer.setRequestLine();
-	TCPQueue.push(testingLayer);
-	
+public:
+	HTTPRequestLine requestLine{};
+	HTTPHeader httpHeader{};
 
+	void setRequestLine() {
+		std::ifstream testingFile("testingInput.txt");
+		if (testingFile.is_open()) {
+			std::string testingString;
+			getline(testingFile, testingString);
+			requestLine.assignValues(testingString);
+		}
+	}
 
-	TransportLayer testingTransport(testingLayer);
-	TCPQueue.pop();
-	TCPQueue.push(testingTransport);
+	void setHeader() {
+		httpHeader.readFile();
+	}
 
-	IPAddress networkLayer(testingTransport);
-	TCPQueue.pop();
-	TCPQueue.push(networkLayer);
-	
-	MacAddress dataLinkLayer(networkLayer);
-	TCPQueue.pop();
-	TCPQueue.push(dataLinkLayer);
-
-	dataLinkLayer.displayFrameFormat();
-
-	//IPAddress IPadresses;
-	//MacAddress Macadresses;
-	//std::string conversionString;
-
-	
-	//testingLayer.httpHeader.readFile();
-	//TCPIPStack.push(testingLayer.httpHeader.getApplicationLayer());
-	//PortAssignment myPortAssignment(testingLayer.httpHeader.getApplicationLayer());
-	//myPortAssignment.assignPorts();
-	//TCPIPStack.push(myPortAssignment.getComboPorts());
-	//IPadresses.setSourceandDestinationIP();
-	//TCPIPStack.push(IPadresses.getComboIPs());
-	//Macadresses.setSourceandDestinationMacAddress();
-	//TCPIPStack.push(Macadresses.getComboMacs());
-
-	//// I could make a function for this
-	//// Conversion string is the variable holding all the information in one variable format
-	//while (!TCPIPStack.empty()) {
-	//	//Original line I used for printing before switching to conversionString
-	//	//std::cout << TCPIPStack.top() << " ";
-	//	conversionString = conversionString + TCPIPStack.top() + " " + "\n";
-	//	TCPIPStack.pop();
-	//}
-	//// Use to print the conversion string as you need for testing purposes and end result
-	//std::cout <<  conversionString;
+	void displayApplicationLayer() {
+		requestLine.displayRequestLine();
+		httpHeader.displayHTTPHeader();
+	}
 
 
-}
+
+
+};
